3f1b837a12ec7bed737da3d597db2a2a4015cc63
Daniel Veramei <danila.veremei@gmail.com>
2022-07-31 18:14:56 +0300

    Initial Commit

---
+++ CurrencyExchangeTask/CurrencyExchangeTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeTaskApp.swift
+//  CurrencyExchangeTask
+//
+//  Created by Daniil Veramei on 31.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
